035871a15394e89757cb61bb41a6ee4df55c34ec
maher ben rhouma <maherbenrhoumaa@gmail.com>
2025-05-20 15:31:15 +0100

    feature(ci/cd) add Code quality assurance

---
+++ ms-learning-backend/ecs.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Operator\ConcatSpaceFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths(
+        [
+        __DIR__ . '/src',
+        __DIR__ . '/tests'
+        ]
+    );
+
+    $ecsConfig->sets(
+        [
+        SetList::PSR_12,
+        SetList::COMMON,
+        SetList::CLEAN_CODE,
+        ]
+    );
+
+    $ecsConfig->skip(
+        [
+        ConcatSpaceFixer::class,
+        ]
+    );
+};
