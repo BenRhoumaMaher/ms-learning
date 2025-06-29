ed7b6b05d3a51a974ccfedac980c00326b41cb1b
maher ben rhouma <maherbenrhoumaa@gmail.com>
2025-06-29 13:37:24 +0100

    feature commit final

--- ms-learning-backend/src/Controller/Quiz/QuizController.php
+++ ms-learning-backend/src/Controller/Quiz/QuizController.php
@@ -11,15 +11,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;   // Security is still useful for general route protection
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class QuizController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-        private ValidatorInterface $validator,
     ) {
     }
 
